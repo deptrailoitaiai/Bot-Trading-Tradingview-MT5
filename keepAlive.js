f31235a909ce64bdb69382508c032ccbdfd4deca
deptrailoitaiai <minh0912873465@gmail.com>
2024-12-24 16:51:36 +0700

    post method

--- keepAlive.js
+++ keepAlive.js
@@ -1,11 +1,11 @@
 const axios = require("axios");
 
 function keepAlive() {
-    try {
-        axios.post("https://bot-trading-tradingview-mt5.onrender.com", { signal: "ping" })
-    } catch (error) {
+    axios
+    .post("https://bot-trading-tradingview-mt5.onrender.com", { signal: "ping" })
+    .catch(() =>{
         console.log("method post not responding");
-    }
+    }) 
 }
 
 setInterval(keepAlive, 3000);
